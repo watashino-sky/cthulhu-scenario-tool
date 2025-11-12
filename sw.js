a8c5305f5005d466e9b98de236469491e0378bea
watashino-sky <may0832sky@gmail.com>
2025-11-12 17:29:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cthulhu-tool-v1';
+const urlsToCache = [
+  './cthulhu-scenario-tool.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
